a8729fdd6b3e11a5786e4b51309d6359862eed10
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-02 18:39:33 +0900

    observe view events

--- app/src/main/java/jp/cordea/ribsdemo/ui/login/LoginInteractor.kt
+++ app/src/main/java/jp/cordea/ribsdemo/ui/login/LoginInteractor.kt
@@ -3,6 +3,11 @@ package jp.cordea.ribsdemo.ui.login
 import com.uber.rib.core.Bundle
 import com.uber.rib.core.Interactor
 import com.uber.rib.core.RibInteractor
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.withLatestFrom
 import javax.inject.Inject
 
 @RibInteractor
@@ -11,9 +16,24 @@ class LoginInteractor : Interactor<LoginInteractor.LoginPresenter, LoginRouter>(
     @Inject
     lateinit var presenter: LoginPresenter
 
+    private val compositeDisposable = CompositeDisposable()
+
     override fun didBecomeActive(savedInstanceState: Bundle?) {
         super.didBecomeActive(savedInstanceState)
+        presenter.clicks()
+            .withLatestFrom(presenter.apiKeyChanges()) { _, key -> key }
+            .filter { it.isNotBlank() }
+            .subscribeBy { }
+            .addTo(compositeDisposable)
     }
 
-    interface LoginPresenter
+    override fun willResignActive() {
+        super.willResignActive()
+        compositeDisposable.clear()
+    }
+
+    interface LoginPresenter {
+        fun apiKeyChanges(): Observable<String>
+        fun clicks(): Observable<Unit>
+    }
 }
--- app/src/main/java/jp/cordea/ribsdemo/ui/login/LoginView.kt
+++ app/src/main/java/jp/cordea/ribsdemo/ui/login/LoginView.kt
@@ -3,9 +3,21 @@ package jp.cordea.ribsdemo.ui.login
 import android.content.Context
 import android.util.AttributeSet
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputLayout
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
+import io.reactivex.Observable
+import jp.cordea.ribsdemo.R
 
 class LoginView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : CoordinatorLayout(context, attrs, defStyleAttr), LoginInteractor.LoginPresenter
+) : CoordinatorLayout(context, attrs, defStyleAttr), LoginInteractor.LoginPresenter {
+    override fun apiKeyChanges(): Observable<String> =
+        findViewById<TextInputLayout>(R.id.api_key).editText!!.textChanges().map { it.toString() }
+
+    override fun clicks(): Observable<Unit> =
+        findViewById<FloatingActionButton>(R.id.fab).clicks()
+}
