7af564e65632ee0a77005a78e2e17d2045fd5b1b
Yoshihiro Tanaka <contact@cordea.jp>
2019-01-03 14:50:53 +0900

    remove unnecessary classes

--- app/src/main/java/jp/cordea/ribsdemo/ui/main/MainActivityModule.kt
+++ app/src/main/java/jp/cordea/ribsdemo/ui/main/MainActivityModule.kt
@@ -4,14 +4,12 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import jp.cordea.ribsdemo.di.ActivityScope
 import jp.cordea.ribsdemo.ui.app.AppFragmentModule
-import jp.cordea.ribsdemo.ui.region.RegionFragmentModule
 
 @Module
 interface MainActivityModule {
     @ActivityScope
     @ContributesAndroidInjector(
         modules = [
-            RegionFragmentModule::class,
             AppFragmentModule::class
         ]
     )
